f89b57976f87f846066cf0eda618138f6642f1c2
Charlotte Chen <hc3558@columbia.edu>
2026-02-19 18:08:26 -0500

    test

--- lab2.c
+++ lab2.c
@@ -29,7 +29,7 @@
  * https://web.archive.org/web/20130307100215/http://beej.us/guide/bgnet/output/html/singlepage/bgnet.html
  *
  * http://www.thegeekstuff.com/2011/12/c-socket-programming/
- * 
+ *
  */
 
 int sockfd; /* Socket file descriptor */
@@ -50,27 +50,32 @@ int main()
   int transferred;
   char keystate[12];
 
-  if ((err = fbopen()) != 0) {
+  if ((err = fbopen()) != 0)
+  {
     fprintf(stderr, "Error: Could not open framebuffer: %d\n", err);
     exit(1);
   }
 
   /* Draw rows of asterisks across the top and bottom of the screen */
-  for (col = 0 ; col < 64 ; col++) {
+  for (col = 0; col < 64; col++)
+  {
     fbputchar('*', 0, col);
+    fbputchar('-', 10, col);
     fbputchar('*', 23, col);
   }
 
-  fbputs("Hello CSEE 4840 World!", 4, 10);
+  fbputs("Testing", 4, 10);
 
   /* Open the keyboard */
-  if ( (keyboard = openkeyboard(&endpoint_address)) == NULL ) {
+  if ((keyboard = openkeyboard(&endpoint_address)) == NULL)
+  {
     fprintf(stderr, "Did not find a keyboard\n");
     exit(1);
   }
-    
+
   /* Create a TCP communications socket */
-  if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) {
+  if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+  {
     fprintf(stderr, "Error: Could not create socket\n");
     exit(1);
   }
@@ -79,13 +84,15 @@ int main()
   memset(&serv_addr, 0, sizeof(serv_addr));
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_port = htons(SERVER_PORT);
-  if ( inet_pton(AF_INET, SERVER_HOST, &serv_addr.sin_addr) <= 0) {
+  if (inet_pton(AF_INET, SERVER_HOST, &serv_addr.sin_addr) <= 0)
+  {
     fprintf(stderr, "Error: Could not convert host IP \"%s\"\n", SERVER_HOST);
     exit(1);
   }
 
   /* Connect the socket to the server */
-  if ( connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+  if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+  {
     fprintf(stderr, "Error: connect() failed.  Is the server running?\n");
     exit(1);
   }
@@ -94,17 +101,20 @@ int main()
   pthread_create(&network_thread, NULL, network_thread_f, NULL);
 
   /* Look for and handle keypresses */
-  for (;;) {
+  for (;;)
+  {
     libusb_interrupt_transfer(keyboard, endpoint_address,
-			      (unsigned char *) &packet, sizeof(packet),
-			      &transferred, 0);
-    if (transferred == sizeof(packet)) {
+                              (unsigned char *)&packet, sizeof(packet),
+                              &transferred, 0);
+    if (transferred == sizeof(packet))
+    {
       sprintf(keystate, "%02x %02x %02x", packet.modifiers, packet.keycode[0],
-	      packet.keycode[1]);
+              packet.keycode[1]);
       printf("%s\n", keystate);
       fbputs(keystate, 6, 0);
-      if (packet.keycode[0] == 0x29) { /* ESC pressed? */
-	break;
+      if (packet.keycode[0] == 0x29)
+      { /* ESC pressed? */
+        break;
       }
     }
   }
@@ -123,7 +133,8 @@ void *network_thread_f(void *ignored)
   char recvBuf[BUFFER_SIZE];
   int n;
   /* Receive data */
-  while ( (n = read(sockfd, &recvBuf, BUFFER_SIZE - 1)) > 0 ) {
+  while ((n = read(sockfd, &recvBuf, BUFFER_SIZE - 1)) > 0)
+  {
     recvBuf[n] = '\0';
     printf("%s", recvBuf);
     fbputs(recvBuf, 8, 0);
@@ -131,4 +142,3 @@ void *network_thread_f(void *ignored)
 
   return NULL;
 }
-
